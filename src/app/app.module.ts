e3e62799f45610b9a18359b3d6b7489b466f38bb
Vajk93 <67112672+Vajk93@users.noreply.github.com>
2024-09-04 10:44:47 +0200

    [MINOR] added signature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CaultKeyComponent } from './components/cault-key/cault-key.component';
 import { ThemeSwitcherComponent } from './components/theme-switcher/theme-switcher.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CaultKeyComponent,
     ThemeSwitcherComponent
   ],
   imports: [
